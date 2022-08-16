7a89345be3b74c1de25123b8d8cd78ac403438a5
mvuksanovic <mvuksanovic@gmail.com>
2022-08-16 20:48:14 +0200

    driver result filter

--- src/components/TeamResults.js
+++ src/components/TeamResults.js
@@ -2,34 +2,44 @@ import React, { useContext } from "react";
 import Flag from './Flag';
 import { Link } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
-import {Table} from "react-bootstrap"
+import { Table } from "react-bootstrap"
 
 const TeamResults = ({ props }) => {
 
+    var driver1 = props[0].Results[0].Driver.familyName;
+    var driver2 = props[0].Results[1] && props[0].Results[1].Driver.familyName;
+
     var team = props.map((data, i) => {
+
+        const driver1Result = data.Results.filter(result => result.Driver.familyName === driver1);
+        const driver2Result = data.Results.filter(result => result.Driver.familyName === driver2);
+        const driver1Pos = (driver1Result.length && driver1Result[0].position) || " ";
+        const driver2Pos = (driver2Result.length && driver2Result[0].position) || " ";
+
         var link = data.Circuit.circuitId
+
         return (
             <tr key={i}>
                 <td className="text-end">{data.round}</td>
                 <td><Flag countryName={data.Circuit.Location.country} />
                     <Link className="text-decoration-none text-reset" to={'/races/' + link}
-                        state = {
-                            {data: data}
+                        state={
+                            { data: data }
                         }
                     > {data.raceName}
                     </Link>
                 </td>
-                <td className="text-end">{data.Results[0].position}</td>
-                <td className="text-end">{data.Results[1] && data.Results[1].position}</td>
+                <td className="text-end">{driver1Result && driver1Pos}</td>
+                <td className="text-end">{driver2Result && driver2Pos}</td>
                 <td className="text-end">{Number(data.Results[0].points) + Number((data.Results[1]) ? (data.Results[1].points) : 0)}</td>
             </tr>
         );
     })
 
-    const {theme} = useContext(ThemeContext)
+    const { theme } = useContext(ThemeContext)
 
     return (
-        <div className={"rounded overflow-auto m-1 p-1 flex-grow-1 bg-"+theme.variant}>
+        <div className={"rounded overflow-auto m-1 p-1 flex-grow-1 bg-" + theme.variant}>
             <Table {...theme}>
                 <thead>
                     <tr><th colSpan="5">Formula 1 {props[0].season} Results</th></tr>
